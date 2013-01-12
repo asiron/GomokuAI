bf1957039b49cf5b78829d13c189b1864135807a
Maciej Żurad <maciej.zurad@gmail.com>
2013-01-12 14:48:15 +0100

    Adding main class

---
+++ src/gomoku/player/Bot.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gomoku.player;
+
+/**
+ *
+ * @author asiron
+ */
+public class Bot {
+    
+}
